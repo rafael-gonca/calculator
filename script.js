6c7336d1407c9b014cee39dafb48e8382afcc3d7
rafael-gonca <143557126+rafael-gonca@users.noreply.github.com>
2025-04-29 14:35:54 -0300

    Add calculator layout and styling

--- script.js
+++ script.js
@@ -30,4 +30,33 @@ function operate(num1, ope, num2) {
     }
 }
 
-console.log(operate(3, '*', 3))
+const colorMap = {
+    0: '#E54B4B',      // C
+    5: '#716F6F',      // ±
+    10: '#716F6F',     // %
+    14: '#716F6F',     // x²
+    15: '#FF7B47',     // ÷
+    16: '#FF7B47',     // ×
+    17: '#FF7B47',     // -
+    18: '#FF7B47',     // +
+    19: '#1985C8'      // =
+  };
+
+const btnsContainer = document.querySelector('.buttons-container')
+index = 0
+for (i = 1; i < 5; i++) {
+    const columnBtn = document.createElement('div')
+    columnBtn.classList = 'columnBtn'
+    btnsContainer.appendChild(columnBtn)
+    for (j = 1; j < 6; j++) {
+        const btn = document.createElement('button')
+        columnBtn.appendChild(btn)
+        btn.classList = 'btn'
+        contentArray = ['C', '7', '4', '1', '0', '±', '8', '5', '2', '.', '%', '9', '6', '3', 'x²', '÷', '×', '-', '+', '=']
+        btn.textContent = contentArray[index]
+        if (colorMap[index]) {
+            btn.style.backgroundColor = colorMap[index];
+          }
+        index++
+    }
+}
